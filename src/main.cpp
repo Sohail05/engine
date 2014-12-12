7905c6762104e3c8d45a5708071c04c81b9a00f0
Stephan Vedder <stephan.vedder@gmail.com>
2014-12-12 17:26:13 +0100

    Did add SFML, edited main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <SFML/Window.hpp>
+
+/**
+* the program entry point
+* @param argc the argument count
+* @param argv the array containing the arguments
+*/
+int main(int argc, char** argv)
+{
+    sf::Window win;
+    while (win.isOpen())
+    {
+
+    }
+    return 0;
+}
