bb84069f3458cfd5018bc74919c159b5a0fb89ba
Damian Dimanov <damian.dimanov@gmail.com>
2014-12-13 12:14:22 +0200

    entry point

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,32 @@
-#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
 
 /**
 * the program entry point
 * @param argc the argument count
 * @param argv the array containing the arguments
 */
-int main(int argc, char** argv)
-{
-    sf::Window win;
-    while (win.isOpen())
-    {
 
-    }
-    return 0;
+using namespace sf;
+
+#define WIDTH 800
+#define HEIGHT 600
+#define TITLE "TITLE"
+
+int main(int argc, char** argv) {
+	RenderWindow window(VideoMode(WIDTH, HEIGHT), TITLE);
+
+	while (window.isOpen()) {
+		Event event;
+		while (window.pollEvent(event)) {
+			if (event.type == Event::Closed) {
+				window.close();
+			}
+		}
+
+		window.clear(Color::Red);
+		//window.draw();
+		window.display();
+	}
+
+    return EXIT_SUCCESS;
 }
