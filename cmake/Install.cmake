3b82d75853b2d68f414ab7952df06c5fb7df9317
Ankit Jain <radical@gmail.com>
2014-12-19 15:35:35 +0530

    Install.cmake: generate app bundle on mac

--- cmake/Install.cmake
+++ cmake/Install.cmake
@@ -15,10 +15,8 @@ if (LINUX OR WINDOWS)
         LIBRARY DESTINATION lib${LIB_SUFFIX} COMPONENT binaries
         ARCHIVE DESTINATION lib${LIB_SUFFIX} COMPONENT libraries)
 else()
-	install(TARGETS ${GAME}
-            FRAMEWORK
-            DESTINATION ${CMAKE_INSTALL_FRAMEWORK_PREFIX}
-            COMPONENT binaries)
+	set_target_properties(${GAME} PROPERTIES MACOSX_BUNDLE TRUE)
+        install(TARGETS ${GAME} BUNDLE DESTINATION ${INSTALL_MISC_DIR}/${target} COMPONENT game)
 endif()
 
 install(FILES "${CMAKE_SOURCE_DIR}/License.txt" "${CMAKE_SOURCE_DIR}/ReadMe.txt" "${CMAKE_SOURCE_DIR}/Authors.txt"
