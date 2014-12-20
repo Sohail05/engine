bc0491ef868a932a938a909c9e4db08def68d969
david-mcgrane <david@davidmcgrane.com>
2014-12-20 16:10:32 +0000

    Comments Corrected
    Comments were displaying on the wrong line, corrected

--- src/Engine.h
+++ src/Engine.h
@@ -8,7 +8,8 @@
 #include "MenuState.h"
 #include "GameState.h"
 
-/* The Engine class is used to store information about the window, main loop, input etc */
+/** The Engine class is used to store information about the window, main loop, input etc */
+
 class Engine
 {
 public:
@@ -16,19 +17,19 @@ public:
 	Engine( std::string Title, int WindowWidth, int WindowHeight, int WindowPositionX, int WindowPositionY );
 	~Engine( void );
 
-	void Initialize();	                     /** Main Initialize Loop */
-	void Update();	                         /** Main Update Loop */
-	void Render( sf::RenderWindow* Window ); /** Main Render Loop @param Handle to a Render Window */
-	void Release();	                         /** Release any memory obtained */
+	void Initialize();	                     ///< Main Initialize Loop
+	void Update();	                         ///< Main Update Loop 
+	void Render( sf::RenderWindow* Window ); ///< Main Render Loop @param Handle to a Render Window 
+	void Release();	                         ///< Release any memory obtained 
 
-	inline void SetGameState( std::unique_ptr<GameState> GameState ) { m_GameState = std::move( GameState ); } 	/** Sets the GameState variable @param unique_pointer to a GameState */
-	inline void SetMenuState( std::unique_ptr<MenuState> MenuState ) { m_MenuState = std::move( MenuState ); } 	/** Sets the MenuState variable @param unique_pointer to a MenuState*/
+	inline void SetGameState( std::unique_ptr<GameState> GameState ) { m_GameState = std::move( GameState ); } 	///< Sets the GameState variable @param unique_pointer to a GameState
+	inline void SetMenuState( std::unique_ptr<MenuState> MenuState ) { m_MenuState = std::move( MenuState ); } 	///< Sets the MenuState variable @param unique_pointer to a MenuState
 
-	inline std::string GetWindowTitle()     { return m_Title;           } /** @returns the Title for the Window */
-	inline int		   GetWindowWidth()     { return m_WindowWidth;     } /** @returns the Width of the Window in pixels */
-	inline int         GetWindowHeight()    { return m_WindowHeight;    } /** @returns the Height of the Window in pixels */
-	inline int         GetWindowPositionX() { return m_WindowPositionX; } /** @returns the Position of the Window along the xAxis */
-	inline int         GetWindowPositionY() { return m_WindowPositionY; } /** @returns the Position of the Window along the yAxis */
+	inline std::string GetWindowTitle()     { return m_Title;           } ///< @returns the Title for the Window 
+	inline int		   GetWindowWidth()     { return m_WindowWidth;     } ///< @returns the Width of the Window in pixels 
+	inline int         GetWindowHeight()    { return m_WindowHeight;    } ///< @returns the Height of the Window in pixels 
+	inline int         GetWindowPositionX() { return m_WindowPositionX; } ///< @returns the Position of the Window along the xAxis 
+	inline int         GetWindowPositionY() { return m_WindowPositionY; } ///< @returns the Position of the Window along the yAxis 
 
 private:
 
--- src/Map.h
+++ src/Map.h
@@ -12,18 +12,18 @@ public:
 	Map( int MapWidth, int MapHeight );
 	~Map(void);
 
-	void Initialize();                       /** Intialize the Map Data */
-	void Update();   	                     /** Update the Map Data */
-	void Render( sf::RenderWindow* Window ); /** Render the Map Data @param Handle to the Render Window */
+	void Initialize();                       ///< Intialize the Map Data
+	void Update();   	                     ///< Update the Map Data
+	void Render( sf::RenderWindow* Window ); ///< Render the Map Data @param Handle to the Render Window
 	
-	inline void SetTileSize( int TileSize ) { m_TileSize = TileSize; } /** Set the Size to Draw Tiles @param Size in Pixels */
+	inline void SetTileSize( int TileSize ) { m_TileSize = TileSize; } ///< Set the Size to Draw Tiles @param Size in Pixels
 
 private:
-	int m_MapWidth;  /** The Width of the Map in Pixels */
-	int m_MapHeight; /** The Height of the Map in Pixels */
-	int m_TileSize;  /** The Size of each tile in Pixels */
+	int m_MapWidth;  ///< The Width of the Map in Pixels
+	int m_MapHeight; ///< The Height of the Map in Pixels
+	int m_TileSize;  ///< The Size of each tile in Pixels
 
-	std::vector<int> m_MapData; /** The indices that hold the value of the maps */
+	std::vector<int> m_MapData; ///< The indices that hold the value of the maps
 
 };
 
